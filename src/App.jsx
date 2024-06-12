5d014710dd159875fd9ef4e89a5fd49eb1aade1d
andresalban <andresalban@unicauca.edu.co>
2024-06-12 10:43:40 -0500

    Aula 2 - Componentes y fuentes iniciales-Terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import styled from "styled-components";
 import GlobalStyles from "./components/GlobalStyles/index.jsx";
+import Cabecera from "./components/Cabecera/Cabecera.jsx";
+import BarraLateral from "./components/BarraLateral/BarraLateral.jsx";
+import Banner from "./components/Banner/Banner.jsx";
+import banner from "./assets/banner.png"
 
-const FondoGradiente= styled.div`
-background: linear-gradient(175deg,#041833 4.16%, #04244f 48%,#154580 96.76%);
+const FondoGradiente = styled.div`
+    background: linear-gradient(175deg, #041833 4.16%, #04244f 48%, #154580 96.76%);
     width: 100%;
     min-height: 100vh;
 `
 
 function App() {
 
-  return (
-    <>
-    <FondoGradiente >
-        <GlobalStyles/>
-    </FondoGradiente>
-    </>
-  )
+    return (
+        <>
+            <FondoGradiente>
+                <GlobalStyles/>
+                <Cabecera/>
+                <BarraLateral/>
+                <Banner texto="La galería más completa de fotos del espacio" backgroundImage={banner}/>
+            </FondoGradiente>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Cabecera/Cabecera.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import CampoTexto from "../CampoTexto/CampoTexto.jsx";
+
+const HeaderEstilizado = styled.header`
+    padding: 60px 0px; 
+    display: flex;
+    justify-content: space-between;
+    
+    
+    img{
+        width: 212px;
+    }
+`
+
+ const Cabecera = () => {
+    return (
+        <HeaderEstilizado>
+            <img src="/imagenes/logo.png" alt="logoSpaceApp" />
+            <CampoTexto/>
+        </HeaderEstilizado>
+    )
+
+}
+
+export default Cabecera
